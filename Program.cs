45ec251fff2b8852b6006dd1f2666d9f91b615f5
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-08-01 19:43:00 -0300

    Exercício Feito

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("\n- Como você faz a galinha dar risada?\n");
+Thread.Sleep(3000);
+Console.WriteLine("Você bate a cara dela na parede...");
+Thread.Sleep(1500);
+Console.WriteLine("...porque desse jeito ela racha o bico.\n");
